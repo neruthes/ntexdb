ee46fb7b14104def7afdd915b5f1f35385e5bf44
Neruthes <i@neruthes.xyz>
2021-09-15 21:49:53 +0000

    Regular update 2021-09-15T10:21:15+00:00

--- githubci.sh
+++ githubci.sh
@@ -3,9 +3,11 @@
 echo "Welcome to <githubci.sh> script."
 
 mkdir -p _dist
+rm -rf _dist
+mkdir -p _dist
 
 # rsync -av rsync://ndvs1.wan.neruthes.xyz/githubdist/ntexdb/ _dist/ --exclude={'tex-tmp','.git'}
 
+rm -f *.tar.xz 2>/dev/null
 wget https://githubdist.neruthes.xyz/ntexdb/dist.tar.xz
-
 tar -pxvf dist.tar.xz
--- upload.sh
+++ upload.sh
@@ -4,6 +4,6 @@
 # proxychains -q 
 # rsync -av --delete _dist/ root@ndvs1.wan.neruthes.xyz:/root/WEB/githubdist/ntexdb/ --exclude={'tex-tmp','.git'}
 
-tar cvf dist.tar.xz --exclude tex-tmp _dist
+tar cvf dist.tar.xz --exclude tex-tmp --exclude .git _dist
 
 scp dist.tar.xz root@ndvs1.wan.neruthes.xyz:/root/WEB/githubdist/ntexdb/dist.tar.xz
