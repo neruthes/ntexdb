fd2ac6076f6315261e664e7d7336ad9cc2b570ec
Neruthes <i@neruthes.xyz>
2021-09-18 19:21:59 +0000

    Added misc/fapiao.tex

--- upload.sh
+++ upload.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # ssh root@ndvs1.wan.neruthes.xyz 'mkdir -p /root/WEB/githubdist/ntexdb'
-# proxychains -q 
 rsync -av --delete _dist/ root@ndvs1.wan.neruthes.xyz:/root/WEB/githubdist/ntexdb/ --exclude={'tex-tmp','.*','private'}
 
 tar cvf dist.tar.xz --exclude tex-tmp --exclude .git _dist
-
 scp dist.tar.xz root@ndvs1.wan.neruthes.xyz:/root/WEB/githubdist/ntexdb/dist.tar.xz
