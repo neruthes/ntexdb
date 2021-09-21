f88ed6d270b037250d92c0edb85dad31024fceba
Neruthes <i@neruthes.xyz>
2021-09-21 23:54:36 +0000

    Regular update 2021-09-21T22:54:09+00:00

--- upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# ssh root@ndvs1.wan.neruthes.xyz 'mkdir -p /root/WEB/githubdist/ntexdb'
-rsync -av --delete _dist/ root@ndvs1.wan.neruthes.xyz:/root/WEB/githubdist/ntexdb/ --exclude={'tex-tmp','.*','private'}
-
-tar cvf dist.tar.xz --exclude tex-tmp --exclude .git _dist
-scp dist.tar.xz root@ndvs1.wan.neruthes.xyz:/root/WEB/githubdist/ntexdb/dist.tar.xz
