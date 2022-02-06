35c27f3b173fe6c715e6adb16b6ec57729dc16e0
Neruthes <i@neruthes.xyz>
2022-02-06 11:12:07 +0000

    Regular update '2022-02-06T11:12:07+00:00'

--- tex.sh
+++ tex.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-FilePath=$1
-BaseName=$(basename $FilePath)
-TYPE_SLASH=${FilePath/$BaseName/}
+FilePath="$1"
+BaseName="$(basename $FilePath)"
+TYPE_SLASH="${FilePath/$BaseName/}"
 
 echo "FilePath=$FilePath"
 echo "BaseName=$BaseName"
 echo "TYPE_SLASH=$TYPE_SLASH"
 
-xelatex -output-directory=_dist/tex-tmp $FilePath
+xelatex -output-directory=_dist/tex-tmp "$FilePath"
 
-mkdir -p _dist/$TYPE_SLASH
-mv _dist/tex-tmp/*.pdf _dist/$TYPE_SLASH
+mkdir -p "_dist/$TYPE_SLASH"
+mv _dist/tex-tmp/*.pdf "_dist/$TYPE_SLASH"
 
 realpath "_dist/${FilePath/.tex/.pdf}"
