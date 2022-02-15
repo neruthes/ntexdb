dd571545e795e37f5b27b36744a563893e4c15c0
Neruthes <i@neruthes.xyz>
2022-02-15 14:52:47 +0000

    Regular update '2022-02-15T14:52:47+00:00'

--- tex.sh
+++ tex.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+XELATEXARGS=""
+#####################################################################
+### Parse arguments. Magic; do not touch.
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --png)
+        TOIMG="png"
+        shift # past argument
+        ;;
+    --jpg)
+        TOIMG="jpg"
+        shift # past argument
+        ;;
+    -*|--*)
+        echo "Unknown option $1"
+        exit 1
+        ;;
+    *)
+        POSITIONAL_ARGS+=("$1") # save positional arg
+        shift # past argument
+        ;;
+  esac
+done
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+#####################################################################
+
+
+
 FilePath="$1"
 BaseName="$(basename $FilePath)"
 TYPE_SLASH="${FilePath/$BaseName/}"
@@ -8,7 +37,10 @@ TYPE_SLASH="${FilePath/$BaseName/}"
 # echo "BaseName=$BaseName"
 # echo "TYPE_SLASH=$TYPE_SLASH"
 
-xelatex -output-directory=_dist/tex-tmp --shell-escape "$FilePath"
+xelatex $XELATEXARGS \
+    -output-directory=_dist/tex-tmp \
+    -shell-escape \
+    "$FilePath"
 
 mkdir -p "_dist/$TYPE_SLASH"
 mv _dist/tex-tmp/*.pdf "_dist/$TYPE_SLASH"
@@ -17,3 +49,10 @@ REALPATH=$(realpath "_dist/${FilePath/.tex/.pdf}")
 du -h "$REALPATH"
 
 saveFileToNasOSS "$REALPATH"
+
+case "$TOIMG" in
+    jpg|png)
+        pdftoimg "$REALPATH" "/tmp/$(basename "$REALPATH").$TOIMG" >/dev/null 2>&1
+        echo "Made image:" "/tmp/$(basename "$REALPATH").$TOIMG"
+        ;;
+esac
